b634854136cd188fb56846446b5f7d7ac231c641
Luã Vilela <luanvilela12@gmail.com>
2021-11-19 16:38:35 -0300

    props

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
+import {PostCard} from './components/PostCard';
 
 class App extends Component{
   // estado
@@ -37,13 +38,20 @@ class App extends Component{
       <section className="container">
         <div className= "posts">     
             {posts.map(post =>( 
-              <div className="post">
-                <img src={post.cover} alt={post.title}/>
-                <div key={post.id} className="post-content">
-                  <h1>{post.title}</h1>
-                  <p>{post.body}</p>
-                </div>
-              </div>  
+              <PostCard
+                cover = {post.cover}
+                key = {post.id}
+                title = {post.title}
+                id = {post.id}
+                body = {post.body}
+              />
+              // <div className="post">
+              //   <img src={post.cover} alt={post.title}/>
+              //   <div key={post.id} className="post-content">
+              //     <h1>{post.title}</h1>
+              //     <p>{post.body}</p>
+              //   </div>
+              // </div>  
             ))}          
         </div>  
       </section>
